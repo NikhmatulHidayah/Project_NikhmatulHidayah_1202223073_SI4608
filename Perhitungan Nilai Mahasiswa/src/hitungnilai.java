c210699333897a9928452c15db86d1030486e514
NikhmatulHidayah <147959798+NikhmatulHidayah@users.noreply.github.com>
2023-11-03 23:29:06 +0700

    Delete Perhitungan Nilai Mahasiswa directory

--- Perhitungan Nilai Mahasiswa/src/hitungnilai.java
+++
@@ -1,9 +0,0 @@
-//nama : nikhmatul hidayah
-//nim : 1202223073
-//kelas : si-46-08
-public class hitungnilai {
-    //fungsi hitung nilai
-    public double hitung(double nilaitubes, double nilaiquiz, double nilaitugas, double nilaiuts,double nilaiuas){
-        return 0.3*nilaitubes + 0.1*nilaiquiz + 0.1*nilaitugas + 0.25*nilaiuts + 0.25*nilaiuas;
-    }
-}
