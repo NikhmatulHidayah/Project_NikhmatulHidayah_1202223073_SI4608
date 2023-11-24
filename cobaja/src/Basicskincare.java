dcf926273a15640e714d37b61483a7fec6e8b341
NikhmatulHidayah <hinikhmatulhidayah@gmail.com>
2023-11-24 18:03:54 +0700

    Perbarui ulang

---
+++ cobaja/src/Basicskincare.java
@@ -0,0 +1,12 @@
+public abstract class Basicskincare {
+    public String merk;
+    public String Harga;
+    public String stok;
+    
+
+void Carabayar() {
+    System.out.println("Cara Bayar"); 
+}
+
+
+}
---
+++ cobaja/src/SkincareCream.java
@@ -0,0 +1,16 @@
+public class SkincareCream extends Basicskincare{
+    public String Malam;
+    public String Pagi;
+
+    void CaraPengunaan() {
+        System.out.println("cara penggunaan Cream pagi/malam"); 
+            } 
+
+    @Override
+    void Carabayar() {
+        System.out.println("diskon jika memakai bank tertentu"); 
+        super.Carabayar();
+    }
+
+
+}
